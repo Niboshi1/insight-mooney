0bbe636f7e6200067644be1568b9c160b6fc8f48
Kotaro Yamashiro <yharding7@gmail.com>
2026-08-24 12:11:36 -0500

    Update instructions on response

--- ptb/phase1_response.m
+++ ptb/phase1_response.m
@@ -22,15 +22,12 @@ function [promptTime, suddennessRating, suddennessTime, confidenceRating, confid
         PsychPortAudio('GetAudioData', pahandle, cfg.answerDuration+2);
         PsychPortAudio('Start', pahandle, 0, 0, 1);
 
-        promptStartTime = GetSecs;
-        while (GetSecs - promptStartTime) < cfg.answerDuration
-            [keyIsDown, ~, keyCode] = KbCheck(-3);
-            if keyIsDown
-                temp = KbName(keyCode);
-                if isequal(temp(1), 'q')
-                    quitNow = true;
-                    return;
-                end
+        while true
+            [~, keyCode] = KbWait(-3, 2);
+            temp = KbName(keyCode);
+
+            if isempty(cfg.triggerkey) || (~isempty(temp) && isequal(temp(1), cfg.triggerkey))
+                break;
             end
         end
         
@@ -40,9 +37,10 @@ function [promptTime, suddennessRating, suddennessTime, confidenceRating, confid
         filename = [cfg.logDir, '/', sprintf('%03d', trial), '_recognition.wav'];
         audiowrite(filename, recordedAudio, 44100);
 
-        % --- Suddenness rating (1 = sudden / popped out, 4 = gradual / figured it out) ---
+        % --- Suddenness rating (1 = sudden / popped out, 5 = gradual / figured it out) ---
         KbReleaseWait(-3);
-        ratetext = 'How did the solution come to you?\n\n1 = sudden (popped out)     4 = gradual (figured it out)';
+        ratetext = ['In a scale of 1-5, how did the solution come to you?\n\n' ...
+                    'sudden (popped out) <-   1   2   3   4   5   -> gradual (figured it out)'];
         DrawFormattedText(window, ratetext, 'center', 'center', .2);
         Screen('Flip', window);
         Eyelink('Message', 'TEXT_PROMPT_SUDDENNESS');
@@ -52,7 +50,7 @@ function [promptTime, suddennessRating, suddennessTime, confidenceRating, confid
             [keyIsDown, ~, keyCode] = KbCheck(-3);
             if keyIsDown
                 temp = KbName(keyCode);
-                if ~isempty(temp) && any(temp(1) == '1234')
+                if ~isempty(temp) && any(temp(1) == '12345')
                     suddennessRating = str2double(temp(1));
                     suddennessTime   = GetSecs - blockStartTime;
                     Eyelink('Message', sprintf('KEY_PRESS_SUDDENNESS_%d', suddennessRating));
@@ -65,9 +63,10 @@ function [promptTime, suddennessRating, suddennessTime, confidenceRating, confid
             end
         end
 
-        % --- Confidence rating (1 = not confident, 4 = very confident) ---
+        % --- Confidence rating (1 = not confident, 5 = very confident) ---
         KbReleaseWait(-3);
-        ratetext = 'How confident are you in your answer?\n\n1 = not confident     4 = very confident';
+        ratetext = ['In a scale of 1-5, how confident are you in your answer?\n\n' ...
+                    'Not confident <-   1   2   3   4   5   -> Very confident'];
         DrawFormattedText(window, ratetext, 'center', 'center', .2);
         Screen('Flip', window);
         Eyelink('Message', 'TEXT_PROMPT_CONFIDENCE');
@@ -77,7 +76,7 @@ function [promptTime, suddennessRating, suddennessTime, confidenceRating, confid
             [keyIsDown, ~, keyCode] = KbCheck(-3);
             if keyIsDown
                 temp = KbName(keyCode);
-                if ~isempty(temp) && any(temp(1) == '1234')
+                if ~isempty(temp) && any(temp(1) == '12345')
                     confidenceRating = str2double(temp(1));
                     confidenceTime   = GetSecs - blockStartTime;
                     Eyelink('Message', sprintf('KEY_PRESS_CONFIDENCE_%d', confidenceRating));
@@ -92,7 +91,7 @@ function [promptTime, suddennessRating, suddennessTime, confidenceRating, confid
 
     else % if not able to solve
         % Prompt for key press TODO: decide on response key
-        resptext = 'Please press key to proceed';
+        resptext = 'Please press "1" key to proceed';
         DrawFormattedText(window, resptext, 'center', 'center', .2);
         Screen('Flip', window);
         Eyelink('Message', 'TEXT_PROMPT_QKEYPRESS'); % log prompt onset to eyelink

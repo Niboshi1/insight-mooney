038170c1ce60a47266f1e984f173932674e44a62
Kotaro Yamashiro <yharding7@gmail.com>
2026-08-28 10:30:06 -0500

    Modify trigger functions to receive message

--- ptb/phase1/phase1_recog.m
+++ ptb/phase1/phase1_recog.m
@@ -7,8 +7,7 @@ function [fixPresentationTime, stimulusPresentationTime, stimPresentationEDFTime
 
     % Fixation show + in center
     draw_cross(window, cfg);
-    Eyelink('Message', 'CROSS_ONSET_PRE_IMG'); % log fixation onset to eyelink
-    tfun(); % send TTL for fixation onset
+    tfun('CROSS_ONSET_PRE_IMG'); % send TTL for fixation onset
     fixPresentationTime = GetSecs - blockStartTime;
 
     % Wait for 3 seconds + jitter
@@ -17,8 +16,7 @@ function [fixPresentationTime, stimulusPresentationTime, stimPresentationEDFTime
     % Mooney image on screen
     Screen('DrawTexture', window, imageTexture);
     [~, imageStart] = Screen('Flip', window);
-    Eyelink('Message', 'IMAGE_ONSET'); % log stimulus onset to eyelink
-    tfun(); % send TTL for stimulus onset
+    tfun('IMAGE_ONSET'); % send TTL for stimulus onset
 
     stimPresentationEDFTime = (Eyelink('TrackerTime')) * 1000;
     stimulusPresentationTime = imageStart - blockStartTime;
@@ -34,15 +32,13 @@ function [fixPresentationTime, stimulusPresentationTime, stimPresentationEDFTime
             if keyIsDown
                 temp = KbName(keyCode);
                 if ~isempty(temp) && isequal(temp(1), cfg.answerkey)
-                    Eyelink('Message', 'KEY_PRESS_INSIGHT'); % log key press to eyelink
-                    sfun(); % send TTL for response
+                    sfun('KEY_PRESS_INSIGHT'); % send TTL for response
                     responseTime = secs - blockStartTime;
                     keyResponse = true;
                 end
             end
         end
     end
-    Eyelink('Message', 'END_STIMULUS'); % log stimulus offset to eyelink
-    tfun(); % send TTL for stimulus offset
+    tfun('END_STIMULUS'); % send TTL for stimulus offset
 
 end
